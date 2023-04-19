e9153d179d2ef4b41d29ce63b66b597de5bfb15b
na-bila <tagueznabila8@gmail.com>
2023-04-19 05:40:26 +0100

    Merge remote-tracking branch 'origin/login_nabila' into login_nabila

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
 // ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors
 
 
-import 'package:firebase_core/firebase_core.dart';
+
 import 'package:flutter/material.dart';
 import 'package:untitled/login_screen.dart';
 
-void main() async{
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+void main() {
+  //WidgetsFlutterBinding.ensureInitialized();
+  //await Firebase.initializeApp();
   runApp( MaterialApp(
     initialRoute: '/loginScreen',
     debugShowCheckedModeBanner: false,
